13c6c8dd8ef2bc9f3d5fd7460a565608518dc936
zlj0402 <2092416525@qq.com>
2025-03-22 20:18:03 +0800

    try to know what size does an empty class or an empty class' object occupy

---
+++ languages/C++/Textbook/Effective_C++/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Empty_Class/Empty.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "load.h"
+
+
+class Empty {};
+
+
+int main(int argc, char* argv[]) {
+
+	std::cout << "sizeof Empty class: " << sizeof(Empty) << std::endl;
+	Empty empty;
+	std::cout << "sizeof Empty class' object: " << sizeof(empty) << std::endl;
+
+	return 0;
+}
---
+++ languages/C++/Textbook/Effective_C++/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Empty_Class/load.h
@@ -0,0 +1,8 @@
+/**
+ * @brief: try to know what size does an empty class or an empty class' object occupy -- 22/3/2025
+ *
+ * @output: 
+ * 		zlj@zlj-vm:~/zljgit/languages/C++/Textbook/Effective_C++/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Empty_Class$ ./Empty
+ * 		sizeof Empty class: 1
+ * 		sizeof Empty class' object: 1
+ */
