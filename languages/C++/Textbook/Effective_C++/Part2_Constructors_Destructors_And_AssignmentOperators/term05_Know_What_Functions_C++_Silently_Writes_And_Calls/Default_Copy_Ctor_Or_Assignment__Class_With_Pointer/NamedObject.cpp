6ba7a9ea6986554dcc4038dda849ef2d37324865
zlj0402 <2092416525@qq.com>
2025-03-27 14:12:46 +0800

    try to find out if one object(its class with pointer member) which is created by default copy ctor, what does the object's pointer member point to

---
+++ languages/C++/Textbook/Effective_C++/Part2_Constructors_Destructors_And_AssignmentOperators/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Default_Copy_Ctor_Or_Assignment__Class_With_Pointer/NamedObject.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "load.h"
+
+template<typename T>
+class NamedObject {
+public:
+	NamedObject(T* obj) {
+		this->objectPointer = obj;
+		std::cout << "ctor called. --- " << (unsigned long)this << std::endl;
+	}
+
+	~NamedObject() {
+		std::cout << "dtor called. --- " << (unsigned long)this << std::endl;
+		delete this->objectPointer;
+	}
+
+private:
+	T* objectPointer;
+
+public:
+	T getVal() const {
+		return *(this->objectPointer);
+	}
+
+	void setVal(T val) {
+		*(this->objectPointer) = val;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	int a = 10;
+	NamedObject<int> no1(&a);
+	std::cout << "*(no1.objectPointer): " << no1.getVal() << std::endl;
+	std::cout << std::endl;
+	
+	NamedObject<int> no2(no1);
+	std::cout << "copy ctor: NamedObject<int> no2(no1);" << std::endl;
+	std::cout << "*(no2.objectPointer): " << no2.getVal() << std::endl;
+	std::cout << std::endl;
+
+	no2.setVal(20);
+	std::cout << "*(no2.objectPointer): " << no2.getVal() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "How about a and *(no1.objectPointer)? " << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "*(no1.objectPointer): " << no1.getVal() << std::endl;
+}
---
+++ languages/C++/Textbook/Effective_C++/Part2_Constructors_Destructors_And_AssignmentOperators/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Default_Copy_Ctor_Or_Assignment__Class_With_Pointer/load.h
@@ -0,0 +1,43 @@
+/**
+ * @brief: try to find out if one object(its class with pointer member) which is created by default copy ctor, what does the object's pointer member point to -- 27/3/2025
+ *
+ * @output:
+ * 		zlj@zlj-vm:~/zljgit/languages/C++/Textbook/Effective_C++/Part2_Constructors_Destructors_And_AssignmentOperators/term05_Know_What_Functions_C++_Silently_Writes_And_Calls/Default_Copy_Ctor_Or_Assignment__Class_With_Pointer$ ./NamedObject
+ * 		ctor called. --- 140724805538696
+ * 		*(no1.objectPointer): 10
+ *
+ * 		copy ctor: NamedObject<int> no2(no1);
+ * 		*(no2.objectPointer): 10
+ *
+ * 		*(no2.objectPointer): 20
+ *
+ * 		How about a and *(no1.objectPointer)?
+ * 		a: 20
+ * 		*(no1.objectPointer): 20
+ * 		dtor called. --- 140724805538704
+ * 		free(): invalid pointer
+ * 		Aborted (core dumped)
+ *
+ * @description:
+ * 		结合程序看，main函数的一开始，创建了a = 10; 并以a的值创建了no1;
+ * 		所以no1.getVal()输出为 10;
+ *
+ * 		no2是默认的拷贝构造的方式创建的;(结合后面，能推定no2的地址为140724805538704)
+ * 		发现指针指向的地方的值，也是10;
+ * 		(那是no2的pointer也指向a，还是no2的pointer指向新的地方，值也为10呢？)
+ *
+ * 		通过no2.setVal(20)修改了，no2的pointer指向的地方的值，现在为20;
+ * 		在看看a和*(no1.objectPointer)值，
+ * 		我们现在可以推定，默认的拷贝构造，就是单纯的一字节一字节的拷贝，所以指针变量的值，也是拷贝过来的，和拷贝构造参数变量的pointer存储的地址是相同的;
+ *
+ * 		正因为如此，no2先被析构掉之后，no1再去析构时，delete this->objectPointer的地址，已经是个无效的地址了;
+ * 		所以，会报错free(): invalid pointer;
+ *
+ * 		+ 为什么能推定140724805538704是no2的地址?
+ * 			NamedObject class的大小，只有一个指针的大小，64位机器的指针大小为8字节，
+ * 			并且no2的创建顺序，紧跟no1的后面，
+ * 			140724805538704 - 140724805538696 = 8
+ *
+ * @summary:
+ * 		默认的拷贝构造，是一字节一字节的复制过来的;包括指针变量存储的地址;
+ */
