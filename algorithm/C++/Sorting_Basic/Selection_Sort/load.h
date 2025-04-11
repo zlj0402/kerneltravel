34d242afba425a289edd5a203c9646a2261745ee
zlj0402 <2092416525@qq.com>
2025-04-11 17:19:36 +0800

    Selection Sort -- C++

---
+++ algorithm/C++/Sorting_Basic/Selection_Sort/load.h
@@ -0,0 +1,9 @@
+/*
+ * @brief: detect time performance of selection sort
+ *
+ * @output:
+ * 		+ Cases count: 20000
+ *
+ * 		zlj@zlj-vm:~/zljgit/algorithm/C++/Sorting_Basic/Selection_Sort$ ./selection_sort
+ * 		Selection Sort : 0.416459 s
+ */
