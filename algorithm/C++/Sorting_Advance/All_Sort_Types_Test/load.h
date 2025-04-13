580bba1a9af8f2a02a37b2d8eb6e87a117cbfbef
zlj0402 <2092416525@qq.com>
2025-04-13 16:18:34 +0800

    cp All types sort algorithm for Sorting_Advance

---
+++ algorithm/C++/Sorting_Advance/All_Sort_Types_Test/load.h
@@ -0,0 +1,35 @@
+/*
+ * @brief: all versions of each sort algorithm && their comparation
+ *
+ * @output: + please update after add a version under the sort type;
+ * 			+ had better sort by time efficientcy
+ * 				+ such as: SelectionSort -> SelectionSort2 -> ... -> SelectionSortn
+ * 					SelectionSortn -> is the best version of Selection Sort
+ * 			+ output as following:
+ */
+
+/* output:
+Test for random array, size = 100000, random range [0, 100000]
+Insertion Sort : 6.64 s
+Shell Sort : 0.022 s
+Shell Sort2 : 0.021 s
+Selection Sort : 10.708 s
+Selection Sort2 : 5.825 s
+Bubble Sort : 25.684 s
+
+Test for more ordered random array, size = 100000, random range [0, 3]
+Insertion Sort : 5.073 s
+Shell Sort : 0.008 s
+Shell Sort2 : 0.006 s
+Selection Sort : 12.468 s
+Selection Sort2 : 7.189 s
+Bubble Sort : 25.399 s
+
+Test for nearly ordered array, size = 100000, swap time = 100
+Insertion Sort : 0.008 s
+Shell Sort : 0.008 s
+Shell Sort2 : 0.006 s
+Selection Sort : 14.481 s
+Selection Sort2 : 8.321 s
+Bubble Sort : 1.86 s
+*/
