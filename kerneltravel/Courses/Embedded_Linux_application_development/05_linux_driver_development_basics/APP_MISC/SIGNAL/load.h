5ed77cf3d42a0121b58bfae7a99a11304ef90d52
zlj0402 <2092416525@qq.com>
2025-04-27 22:46:54 +0800

    a simple signal app test

---
+++ kerneltravel/Courses/Embedded_Linux_application_development/05_linux_driver_development_basics/APP_MISC/SIGNAL/load.h
@@ -0,0 +1,3 @@
+/*
+ * @brief: a simple signal app test -- porting on 27/4/2025
+ */
---
+++ kerneltravel/Courses/Embedded_Linux_application_development/05_linux_driver_development_basics/APP_MISC/SIGNAL/signal_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "load.h"
+
+void my_sig_func (int signo)
+{
+	printf("get a signal : %d\n", signo) ;
+}
+
+int main (int argc, char ** argv)
+{
+	int i = 0;
+	signal(SIGIO, my_sig_func);
+	while (1)
+	{
+		printf("Hello, world %d!\n", i++);
+		sleep(2);
+	}
+	return 0;	
+}
