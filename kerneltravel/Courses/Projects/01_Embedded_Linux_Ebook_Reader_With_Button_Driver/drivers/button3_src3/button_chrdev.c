d90d345f2d25144ced5dbf61f199064225a721d1
zlj0402 <2092416525@qq.com>
2025-10-09 19:10:07 +0800

    correct button driver -- seperated

--- kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button3_src3/button_chrdev.c
+++ kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button3_src3/button_chrdev.c
@@ -2,11 +2,14 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/poll.h>
+#include <linux/platform_device.h>
 #include "button_core.h"
 
+#define CHRDEV_CLS_NAME "reader_button_class"
 #define CHRDEV_NAME "reader_button"
 
 static int major;
+static struct class *reader_button_class;
 
 static ssize_t button_read(struct file *file, char __user *buf, size_t size, loff_t *off)
 {
@@ -25,20 +28,33 @@ static unsigned int button_poll(struct file *file, poll_table *wait)
 }
 
 static const struct file_operations button_fops = {
+	
     .owner = THIS_MODULE,
     .read = button_read,
     .poll = button_poll,
 };
 
-static int __init button_chrdev_init(void)
-{
+static int __init button_chrdev_init(void) {
+	
     major = register_chrdev(0, CHRDEV_NAME, &button_fops);
     printk("button chrdev major: %d\n", major);
+	
+	reader_button_class = class_create(THIS_MODULE, CHRDEV_CLS_NAME);
+	if (IS_ERR(reader_button_class)) {
+		printk("%s class create failed.\n", CHRDEV_CLS_NAME);
+		return PTR_ERR(reader_button_class);
+	}
+	else {
+		printk("%s class create successfully.\n", CHRDEV_CLS_NAME);
+	}
+	device_create(reader_button_class, NULL, MKDEV(major, 0), NULL, CHRDEV_NAME);
     return 0;
 }
 
-static void __exit button_chrdev_exit(void)
-{
+static void __exit button_chrdev_exit(void) {
+	
+	device_destroy(reader_button_class, MKDEV(major, 0));
+	class_destroy(reader_button_class);
     unregister_chrdev(major, CHRDEV_NAME);
 }
 
