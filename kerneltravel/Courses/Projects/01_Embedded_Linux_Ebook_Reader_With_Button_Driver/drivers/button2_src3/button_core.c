08d59d78e46719778bd58de95cd9c33e1d511e81
zlj0402 <2092416525@qq.com>
2025-10-09 20:23:29 +0800

    current button driver2 -- seperate -- built to one .ko file

--- kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button2_src3/button_core.c
+++ kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button2_src3/button_core.c
@@ -1,6 +1,9 @@
 #include <linux/sched.h>
+#include <linux/module.h>
 
 #include "button_core.h"
+
+#define NEXT_POS(x) ((x+1)%KEY_BUF_SIZE)
 
 struct button_core g_button_core;
 
@@ -16,9 +19,10 @@ bool button_core_empty(struct button_core *core)
     return core->front == core->rear;
 }
 
+// front -> 写指针; reart -> 读指针;
 static bool button_core_full(struct button_core *core)
 {
-    return ((core->rear + 1) % KEY_BUF_SIZE) == core->front;
+    return NEXT_POS(core->front) == core->rear;
 }
 
 void button_core_put_key(struct button_core *core, int key)
@@ -26,8 +30,9 @@ void button_core_put_key(struct button_core *core, int key)
     unsigned long flags;
     spin_lock_irqsave(&core->lock, flags);
     if (!button_core_full(core)) {
-        core->buf[core->rear] = key;
-        core->rear = (core->rear + 1) % KEY_BUF_SIZE;
+        core->buf[core->front] = key;
+        core->front = NEXT_POS(core->front);
+		printk("\tput key: %d\n", key);
         wake_up_interruptible(&core->waitq);
     }
     spin_unlock_irqrestore(&core->lock, flags);
@@ -39,10 +44,10 @@ int button_core_get_key(struct button_core *core)
     unsigned long flags;
     spin_lock_irqsave(&core->lock, flags);
     if (!button_core_empty(core)) {
-        key = core->buf[core->front];
-        core->front = (core->front + 1) % KEY_BUF_SIZE;
+        key = core->buf[core->rear];
+        core->rear = NEXT_POS(core->rear);
+		printk("\tget key: %d\n", key);
     }
     spin_unlock_irqrestore(&core->lock, flags);
     return key;
 }
-
--- kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button2_src3/button_platform.c
+++ kerneltravel/Courses/Projects/01_Embedded_Linux_Ebook_Reader_With_Button_Driver/drivers/button2_src3/button_platform.c
@@ -6,50 +6,97 @@
 #include "button_core.h"
 #include "load.h"
 
+#define ONE_DAY_JIFFIES (24U * 60U * 60U * 100U)
+
 struct gpio_key {
+	
     int idx;
     int gpio;
-    struct gpio_desc *gpiod;
     int irq;
+    struct gpio_desc *gpiod;
+	enum of_gpio_flags flags;
 };
 
 static struct gpio_key *gpio_keys;
+static unsigned int g_last_key_time;
+static unsigned int g_last_key_val;
 
 static irqreturn_t gpio_key_irq_handler(int irq, void *dev_id)
 {
-    struct gpio_key *key = dev_id;
-    int val = gpiod_get_value(key->gpiod);
-    unsigned int code = (jiffies << 8) | (key->gpio << 4) | val;
-
-    printk("irq %d gpio %d val %d key %u\n", irq, key->gpio, val, code);
-    button_core_put_key(&g_button_core, code);
+	unsigned int key;
+	unsigned int now;
+    struct gpio_key *g_key = dev_id;
+    int val = gpiod_get_value(g_key->gpiod);
+	
+	if (val == 0) {
+		return IRQ_HANDLED;
+	}
+	
+	now = jiffies % ONE_DAY_JIFFIES;
+	key = (now << 8) | (g_key->gpio << 4) | (val << g_key->idx);
+	printk("irq %d, gpio %d, val %d, now: %u, key %u\n\n", irq, g_key->gpio, val, now, key);
+    button_core_put_key(&g_button_core, key);
+	
+	if (g_last_key_val && g_last_key_val != (val << g_key->idx)) {
+		
+		printk("last key time: %u now: %u, diff: %u\n", g_last_key_time, now, now - g_last_key_time);
+	}
+	
+	g_last_key_time = now;
+	g_last_key_val = val << g_key->idx;
+	
     return IRQ_HANDLED;
 }
 
 static int gpio_irq_probe(struct platform_device *pdev)
 {
 	int err;
+	enum of_gpio_flags flags;
     struct device_node *node = pdev->dev.of_node;
     int count = of_gpio_count(node);
     int i;
-
-	button_core_init(&g_button_core);
+	int gpio;
+	int irq;
+	
+	button_core_init(&g_button_core);   // 初始化核心
 	button_chrdev_init();
+	
+	if (!count) {
+		printk("%s %s line %d, there isn't any gpio available\n", __FILE__, __FUNCTION__, __LINE__);
+	} else {
+		printk("gpio count: %d\n", count);
+	}
 
     gpio_keys = devm_kzalloc(&pdev->dev, count * sizeof(*gpio_keys), GFP_KERNEL);
     for (i = 0; i < count; i++) {
-        int gpio = of_get_gpio(node, i);
-        int irq = gpio_to_irq(gpio);
+		
+		gpio = of_get_gpio_flags(node, i, &flags);
+        irq = gpio_to_irq(gpio);
 
-        gpio_keys[i].gpio = gpio;
+		gpio_keys[i].idx   = i;
+        gpio_keys[i].gpio  = gpio;
         gpio_keys[i].gpiod = gpio_to_desc(gpio);
-        gpio_keys[i].irq = irq;
+        gpio_keys[i].irq   = irq;
+		gpio_keys[i].flags = flags;
+		
+		if (flags & OF_GPIO_ACTIVE_LOW) {
+		    // 按键按下时电平为低，设置为低电平有效
+    		flags |= GPIOF_ACTIVE_LOW;  // GPIOF_ACTIVE_LOW -> 在gpio系统中用来标识低电平有效
+		}
 
-        devm_gpio_request_one(&pdev->dev, gpio, GPIOF_IN, NULL);
+        err = devm_gpio_request_one(&pdev->dev, gpio, GPIOF_IN, NULL);
+		if (err < 0) {
+			
+			dev_err(&pdev->dev, "Failed to request GPIO %d, error %d\n",
+				gpio, err);
+			return err;
+		}
+		
+		printk("\tgpio %d irq %d\n", gpio, irq);
+		
         err = request_irq(irq, gpio_key_irq_handler, IRQF_TRIGGER_RISING, "zlj_gpio_key", &gpio_keys[i]);
     }
-
-    button_core_init(&g_button_core);
+	
     return 0;
 }
 
@@ -58,7 +105,7 @@ static int gpio_irq_remove(struct platform_device *pdev)
     struct device_node *node = pdev->dev.of_node;
     int count = of_gpio_count(node);
     int i;
-
+	
 	button_chrdev_exit();
 
     for (i = 0; i < count; i++)
